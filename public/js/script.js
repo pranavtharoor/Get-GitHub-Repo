92eaa3af872a52340884e24256e8e63a5d7c9c31
pranavtharoor <pranav.tharoor@gmail.com>
2017-03-30 22:30:57 +0530

    added folder-up button

--- public/js/script.js
+++ public/js/script.js
@@ -1,6 +1,13 @@
+var pwd = '', user = '', pwt = '';
+
 $('#submit').click(() => {
+  user = $('#user').val()
+  getrepos();
+});
+
+function getrepos() {
 	$.ajax({
-    	url:'/github/' + $('#user').val(),
+    	url:'/github/' + user,
    		type: 'get',
    		success: (data) => {
    			$('#repos #file-list').text('');
@@ -11,10 +18,12 @@ $('#submit').click(() => {
    			for(x in data) {
     			$('#repos #file-list').append('<div class="list-item"><button class="' + data[x].type + ' icon" onclick="sendurl(\'' + data[x].url + '\', \'' + data[x].type + '\')"></button><div class="list-name">' + data[x].name + '</div></div>');
     		}
-		}
+    }
     });
-});
+}
+
 function sendurl(url1, type1) {
+  pwt = type1;
 	$.ajax({
     	url:'/github/type/' + encodeURIComponent(url1) + '/' + type1,
    		type: 'get',
@@ -33,8 +42,28 @@ function sendurl(url1, type1) {
             $('#file-list').removeClass('show-code');
           for(x in data) {
             $('#repos #file-list').append('<div class="list-item"><button class="' + data[x].type + ' icon" onclick="sendurl(\'' + data[x].url + '\', \'' + data[x].type + '\')"></button><div class="list-name">' + data[x].name + '</div></div>');
-	    		}
-	    	}
+          }
+        pwd = data[0].url;
+        }
 		}
     });
 }
+
+$('.folder-up').click(() => {
+  if(pwt == 'file' || pwt == 'repo' || pwt == 'dir') {
+    pwd = pwd.split("/");
+  var length = pwd.length;
+  }
+  if(pwd[length - 2] == 'contents') {
+    getrepos();
+    pwd = pwd.join("/");
+  } else if(pwt == 'file'){
+    pwd.splice(-1, 1);
+    pwd = pwd.join("/");
+    sendurl(pwd, 'dir');
+  } else if(pwt == 'repo' || pwt == 'dir') {
+    pwd.splice(-2, 2);
+    pwd = pwd.join("/");
+    sendurl(pwd, "dir");
+  }
+});
